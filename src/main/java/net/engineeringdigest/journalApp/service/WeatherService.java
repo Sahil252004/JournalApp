3844e8d31cc6e8c79df35adda15be5b3ed968d6f
Sahil252004 <sahilamrutkar1808@gmail.com>
2025-02-04 22:56:07 +0530

    Redis Integration

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -24,12 +24,29 @@ public class WeatherService {
     @Autowired
     private AppCache appCache;
 
+    @Autowired
+    private RedisService redisService;
+
     public WeatherResponse getWeather(String city)
     {
-        String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(PlaceHolders.CITY, city).replace(PlaceHolders.API_KEY,apikey);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
-        WeatherResponse body = response.getBody();
-        return body;
+
+        WeatherResponse weatherResponse = redisService.get("Weather_Of_" + city, WeatherResponse.class);
+        if(weatherResponse != null)
+        {
+            return weatherResponse;
+        }
+        else
+        {
+            String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(PlaceHolders.CITY, city).replace(PlaceHolders.API_KEY,apikey);
+            ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+            WeatherResponse body = response.getBody();
+            if(body != null)
+            {
+                redisService.set("Weather_Of_" + city,body,300l);
+            }
+            return body;
+        }
+
 
     }
 }
